fe44e6c71b00aade801615a42f2a4976ee135879
victormironov68 <bart.68mail@gmail.com>
2023-07-01 20:22:52 +0300

    изменения 01.07.2023

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,5 @@
+
+
 class BasePage():
     def __init__(self, browser, url):
         self.browser = browser
--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,10 @@
-from selenium import webdriver
-import time
-from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
+
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
-    time.sleep(10)
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
